d3a2e8110123e26eb5c04b26f184ba0ffe9139b8
naturefx <alsmrqs@outlook.com>
2025-04-24 20:47:17 -0300

    trying - readable syntax (step ::)

--- Automaton.hs
+++ Automaton.hs
@@ -9,17 +9,24 @@ import Test
 
 data State a = State 
     { isFinal     :: Bool 
-    , transitions :: a } deriving (Show) -- [Transition] } deriving (Show)
+    , transitions :: a 
+    } deriving (Show) 
 
 instance Functor State where
     fmap f (State b t) = State b (f t)
 
-popTransition :: State [Transition] -> State [Transition]
-popTransition s | null (transitions s) = s | otherwise = fmap tail s
+nextState :: State [Transition] -> Maybe (State [Transition])
+nextState s | null (transitions s) = Nothing | otherwise = Just (getState . head $ transitions s) 
+
+nextState :: State [Transition] -> Maybe (State [Transition])
+nextState s = if null (transitions s) 
+    then Nothing
+    else Just (getState . head $ transitions s)
 
 data Transition = Transition 
     { alphabet :: [Char] 
-    , getState :: State [Transition] } deriving (Show)
+    , getState :: State [Transition] 
+    } deriving (Show)
 
 signal  = State False [Transition ['+','-']  number0]
 number0 = State True  [Transition ['0'..'9'] number1]
@@ -27,16 +34,24 @@ number1 = State True  [Transition ['0'..'9'] number1, Transition ['.'] number2]
 number2 = State False [Transition ['0'..'9'] number3] 
 number3 = State True  [Transition ['0'..'9'] number3]
 
-step :: State [Transition] -> Char -> Maybe (State [Transition])
-step currentState character = 
-    if null (transitions currentState) 
-        then Nothing
-        else 
-            let possibleTransitions@(headTransition:tailTransitions) = transitions currentState 
-            in
-                if character `elem` (alphabet headTransition)
-                    then Just $ getState headTransition
-                    else step (popTransition currentState) character
+--step :: State [Transition] -> Char -> Maybe (State [Transition])
+--step currentState character | null (transitions currentState) = Nothing
+--    | otherwise = 
+--        if character `elem` (alphabet . head $ transitions currentState)
+--            then Just $ getState . head $ transitions currentState
+--            else step (fmap tail currentState) character
+
+--step :: State [Transition] -> Char -> Maybe (State [Transition]) 
+--step currentState character | null (transitions currentState) = Nothing
+--step currentState character | elem character $ alphabet . head $ transitions currentState = Just $ getState . head $ transitions currentState
+--    | otherwise = step (fmap tail currentState) character
+
+step :: State [Transition] -> Char -> Maybe (State [Transition]) 
+step currentState character 
+    | null (transitions currentState) = Nothing
+        -- This line should be summarized
+    | elem character (alphabet . head $ transitions currentState) = nextState currentState -- Just (getState . head $ transitions currentState)
+    | otherwise = step (fmap tail currentState) character
 
 lexer :: [Char] -> State [Transition] -> ([Char], [Char])
 lexer []       _ = ([], [])
