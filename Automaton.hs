4243bee9884fe4f44eef163b10e3f365b2279677
naturefx <alsmrqs@outlook.com>
2025-04-25 10:25:01 -0300

    just renaming thing - readable code

--- Automaton.hs
+++ Automaton.hs
@@ -15,13 +15,13 @@ data State a = State
 instance Functor State where
     fmap f (State b t) = State b (f t)
 
-nextState :: State [Transition] -> Maybe (State [Transition])
-nextState s | null (transitions s) = Nothing | otherwise = Just (getState . head $ transitions s) 
+next :: State [Transition] -> Maybe (State [Transition])
+next s | null (transitions s) = Nothing | otherwise = Just (getState . head $ transitions s) 
 
-nextState :: State [Transition] -> Maybe (State [Transition])
-nextState s = if null (transitions s) 
-    then Nothing
-    else Just (getState . head $ transitions s)
+--next :: State [Transition] -> Maybe (State [Transition])
+--next s = if null (transitions s) 
+--    then Nothing
+--    else Just (getState . head $ transitions s)
 
 data Transition = Transition 
     { alphabet :: [Char] 
@@ -34,32 +34,20 @@ number1 = State True  [Transition ['0'..'9'] number1, Transition ['.'] number2]
 number2 = State False [Transition ['0'..'9'] number3] 
 number3 = State True  [Transition ['0'..'9'] number3]
 
---step :: State [Transition] -> Char -> Maybe (State [Transition])
---step currentState character | null (transitions currentState) = Nothing
---    | otherwise = 
---        if character `elem` (alphabet . head $ transitions currentState)
---            then Just $ getState . head $ transitions currentState
---            else step (fmap tail currentState) character
-
---step :: State [Transition] -> Char -> Maybe (State [Transition]) 
---step currentState character | null (transitions currentState) = Nothing
---step currentState character | elem character $ alphabet . head $ transitions currentState = Just $ getState . head $ transitions currentState
---    | otherwise = step (fmap tail currentState) character
-
-step :: State [Transition] -> Char -> Maybe (State [Transition]) 
-step currentState character 
-    | null (transitions currentState) = Nothing
-        -- This line should be summarized
-    | elem character (alphabet . head $ transitions currentState) = nextState currentState -- Just (getState . head $ transitions currentState)
-    | otherwise = step (fmap tail currentState) character
+step :: Char -> State [Transition] -> Maybe (State [Transition]) 
+step character state 
+    | null (transitions state) = Nothing
+        -- sumarize next line
+    | elem character (alphabet . head $ transitions state) = next state 
+    | otherwise = step character (fmap tail state) 
 
 lexer :: [Char] -> State [Transition] -> ([Char], [Char])
 lexer []       _ = ([], [])
-lexer l@(x:xs) currentState = if isNothing $ step currentState x 
+lexer l@(x:xs) state = if isNothing $ step x state
     then ([], l)
     else 
-        let state  = fromJust $ step currentState x
-            (a, b) = lexer xs state
+        let nxtState = fromJust $ step x state
+            (a, b)   = lexer xs nxtState
         in 
             if null a
                 then bool ([], l) (x:[], xs) $ isFinal state
