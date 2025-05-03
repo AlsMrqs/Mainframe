ad799831502aa9eb415a23c97ba1bcadfe628c22
naturefx <alsmrqs@outlook.com>
2025-05-03 03:33:44 -0300

    runTest - (parser)

--- Main.hs
+++ Main.hs
@@ -1,28 +1,9 @@
 module Main where
 
 import Prelude hiding (read)
-import Data.Maybe
-import Data.List
-import Graph
-import Automaton
+import Data.Either
 import Test
+import Parser
 
 main :: IO ()
-main = do
-    generation <- (sequence . take 80 $ cycle [genNumber])
-
-    (\(a,b) -> mapM_ (putStrLn . show) [a,b]) 
-        . partition ((/=) "" . fst) 
-        . map lexer $ generation
-
-    putStrLn ""
-    print generation
-
-lexer :: [Char] -> ([Char], [Char])
-lexer [] = ([], [])
-lexer l@(x:xs) 
-    | isNothing automaton = ([], l) 
-    | otherwise = read l $ fromJust automaton
-    where
-        automaton = start x
-
+main = runTest
