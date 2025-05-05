1f5ae98e03b53dedc931a9575600c19834bff53f
naturefx <alsmrqs@outlook.com>
2025-05-05 07:44:27 -0300

    resolve - token vs symbol

--- Graph/Grammar.hs
+++ Graph/Grammar.hs
@@ -34,3 +34,20 @@ match token prod
     where
         equal = readable
 
+checker :: Token -> Symbol -> Maybe ([Char], [Symbol])
+checker token symbol = case symbol of
+    (Terminal term) -> if readable term token
+        then Just ([], [])
+        else Nothing
+    (Variable rule) -> let production = find (match token) rule in
+        if isJust production
+            then fmap ((,) token . chain) production
+            else 
+                if all (isVariable . head . chain) rule
+                    then Just $ ((,) token . (chain . head)) rule
+                    else 
+                        if any (null . chain) rule
+                            then Just (token, [])
+                            else Nothing
+
+
