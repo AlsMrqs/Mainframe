af5ac536e71d39c0077133d3058be11bd82cf606
naturefx <alsmrqs@outlook.com>
2025-07-08 23:46:42 -0300

    inverse matrix

--- Math/Matrix/Matrix.hs
+++ Math/Matrix/Matrix.hs
@@ -2,11 +2,10 @@ module Matrix where
 
 import Data.Maybe
 import Data.List
+import Data.Bool
+import Wheel
 
-type Matrix = [[Double]]
-type Coordinates = (Int,Int)
-
-term :: (Int, Int) -> [[Double]]-> Maybe Double
+term :: Num a => (Int, Int) -> [[a]]-> Maybe a
 term (x,y) _       | x < 1 || y < 1  = Nothing
 term _     []      = Nothing
 term (1,y) ([]:ls) = Nothing
@@ -14,45 +13,62 @@ term (1,1) (l:ls)  = Just (head l)
 term (1,y) (l:ls)  = term (1,y-1) (tail l: ls)
 term (x,y) (l:ls)  = term (x-1,y) ls
 
-isSquare :: [[Double]] -> Bool
+isSquare :: Num a => [[a]] -> Bool
 isSquare [] = False
 isSquare ls = all (== length ls) $ map length ls
 
-sarrus :: [[Double]] -> Double
-sarrus mtrx = sarrus' (i,i) (i,n) n 0 (map (\ls -> ls ++ (init ls)) mtrx)
-    where
-    i = 1
-    n = length mtrx
-    sarrus' :: (Int,Int) -> (Int,Int) -> Int -> Double -> [[Double]] -> Double
-    sarrus' (a,b) (c,d) n acc mtrx = if b > n then acc 
-        else 
-            let pos = posProd (a,b) n 1 mtrx 
-                neg = negProd (c,d) n 1 mtrx in
-            sarrus' (a,b+1) (c,d+1) n (acc + pos - neg) mtrx
+isSingular :: (Eq a, Num a) => [[a]] -> Bool
+isSingular = (==) 0 . det
 
-posProd :: (Int,Int) -> Int -> Double -> [[Double]] -> Double
-posProd (i,j) n acc mtrx = if i > n then acc 
-    else posProd (i+1,j+1) n (fromJust (term (i,j) mtrx) * acc) mtrx
+posProd :: Num a => (Int,Int) -> Int -> a -> [[a]] -> a
+posProd (i,j) n acc mtrx = if i > n then acc else 
+    posProd (i+1,j+1) n (fromJust (term (i,j) mtrx) * acc) mtrx
 
-negProd :: (Int,Int) -> Int -> Double -> [[Double]] -> Double
-negProd (i,j) n acc mtrx = if i > n then acc 
-    else negProd (i+1,j-1) n (fromJust (term (i,j) mtrx) * acc) mtrx
+negProd :: Num a => (Int,Int) -> Int -> a -> [[a]] -> a
+negProd (i,j) n acc mtrx = if i > n then acc else 
+    negProd (i+1,j-1) n (fromJust (term (i,j) mtrx) * acc) mtrx
 
-minor :: (Int,Int) -> [[Double]] -> [[Double]]
+minor :: Num a => (Int,Int) -> [[a]] -> [[a]]
 minor (i,j) = map (removeAt (j-1)) . removeAt (i-1) 
     where
     removeAt :: Int -> [a] -> [a]
     removeAt x ls = take x ls ++ drop (x+1) ls
 
-cofactor :: (Int,Int) -> [[Double]] -> Double
-cofactor (i,j) mtrx = ((-1)^(i+j)) * (det . minor (i,j) $ mtrx)
+cof :: Num a => (Int,Int) -> [[a]] -> a
+cof (i,j) = (*) ((-1)^(i+j)) . det . minor (i,j)
 
-det :: [[Double]] -> Double
+det :: Num a => [[a]] -> a
 det mtrx = case length mtrx of
     1 -> head . head $ mtrx
     2 -> posProd (1,1) 2 1 mtrx - negProd (1,2) 2 1 mtrx
-    3 -> sarrus mtrx
     _ -> sum 
-        . map (\j -> (*) (head mtrx !! (j-1)) $ cofactor (1,j) mtrx) 
+        . map (\j -> (*) (head mtrx !! (j-1)) $ cof (1,j) mtrx) 
             $ [1..length mtrx] 
+
+identity :: Num a => [[a]] -> [[a]]
+identity mtrx = map line [0..(length mtrx)-1]
+    where
+    line :: Num a => Int -> [a]
+    line idx = take (length mtrx) $ replicate idx 0 ++ [1] ++ cycle [0]
+
+divide :: Fractional a => [[a]] -> a -> [[a]]
+divide []     _ = []
+divide (x:xs) y = map (/y) x : divide xs y
+
+inverse :: Fractional a => [[a]] -> [[a]]
+inverse mtrx = let det' = det mtrx in map (map (/det')) $ cofactor (transpose mtrx)
+
+cofactor :: Num a => [[a]] -> [[a]]
+cofactor mtrx = aux 1 (uncons mtrx)
+    where
+    aux i idx = case idx of
+        Nothing     -> []
+        Just (x,xs) -> map (\j -> cof (i,j) mtrx) [1..length x] : aux (i+1) (uncons xs)
+
+prod :: Num a => [[a]] -> [[a]] -> [[a]]
+prod m1 m2 = aux (uncons m1) $ transpose m2
+    where
+    aux ref mtrx = case ref of
+        Nothing     -> []
+        Just (x,xs) -> map (\ln -> sum $ zipWith (*) x ln) mtrx : aux (uncons xs) mtrx
 
