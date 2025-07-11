a7a8248973b43996bbefac561e7bf675ba300e3f
naturefx <alsmrqs@outlook.com>
2025-07-11 12:16:22 -0300

    removing (render,control) codes

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLUT
+import Engine.Control
+import Engine.Render
 
 -- Start --
 main :: IO ()
@@ -12,28 +14,3 @@ main = do
     keyboardMouseCallback $= Just (keyboardMouse)
     mainLoop
     
--- Animation --
-type Point = (Double, Double, Double)
-type Vector = Point
-
-render :: DisplayCallback
-render = do
-    clear [ColorBuffer]
-    color $ Color3 1.0 1.0 (1.0 :: GLfloat)
-    renderPrimitive Lines . mapM_ (draw . pointToGLPoint) $ 
-        [(-0.5,0,0),(0.5,0,0),(0,-0.5,0),(0,0.5,0),(0,0,-0.5),(0,0,0.5)]
-    swapBuffers 
-    postRedisplay Nothing
-    where
-        draw :: (GLfloat, GLfloat, GLfloat) -> DisplayCallback 
-        draw (x,y,z) = vertex $ Vertex3 x y z
-
-        pointToGLPoint :: Point -> (GLfloat, GLfloat, GLfloat)
-        pointToGLPoint (x,y,z) = (realToFrac x, realToFrac y, realToFrac z)
-
--- Control --
-keyboardMouse key keyState _ pos = do
-    case (key,keyState) of
-        (Char 'q', Down) -> leaveMainLoop
-        _                -> return ()
-
